8136f07fed3e62bb4e29711d9eb1cb2e7bb7490e
lonecale <54177993+lonecale@users.noreply.github.com>
2026-06-23 17:11:12 +0800

    Update 02_target_only.sh

--- SCRIPTS/X86/02_target_only.sh
+++ SCRIPTS/X86/02_target_only.sh
@@ -38,6 +38,60 @@ sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic
 # 预配置一些插件
 cp -rf ../PATCH/files ./files
 
+### X86 默认设置 ###
+# 生成首次启动执行的 uci-defaults 脚本
+
+DEFAULT_SETTINGS="./files/etc/uci-defaults/99-x86-defaults"
+mkdir -p "$(dirname "$DEFAULT_SETTINGS")"
+
+cat > "$DEFAULT_SETTINGS" <<'EOF'
+#!/bin/sh
+
+### 默认主题 ###
+uci -q set luci.main.mediaurlbase='/luci-static/kucat'
+uci -q commit luci
+
+### 主机名映射 ###
+uci -q add dhcp domain
+uci -q set dhcp.@domain[-1].name='time.android.com'
+uci -q set dhcp.@domain[-1].ip='203.107.6.88'
+
+### DHCP 设置 ###
+# 旁路网关模式：主路由负责 DHCP，本机不分配 IPv4 地址
+#uci -q set dhcp.lan.ignore='1'
+#uci -q set dhcp.lan.dynamicdhcp='0'
+
+# 关闭 IPv6 RA、DHCPv6、NDP，避免本机在 LAN 侧下发 IPv6 配置
+#uci -q delete dhcp.lan.ra
+#uci -q delete dhcp.lan.dhcpv6
+#uci -q delete dhcp.lan.ra_management
+#uci -q delete dhcp.lan.ndp
+
+#uci -q commit dhcp
+
+### 网络设置 ###
+# 旁路网关模式：本机默认网关和 DNS 指向主路由
+#uci -q set network.lan.gateway='10.0.0.2'
+
+#uci -q delete network.lan.dns
+#uci -q add_list network.lan.dns='10.0.0.2'
+
+# 关闭 LAN 口 IPv6 前缀委托和 IPv6 分配
+#uci -q set network.lan.delegate='0'
+#uci -q delete network.lan.ip6assign
+#uci -q set network.lan.ip6ifaceid='eui64'
+
+# 旁路网关不使用默认 WAN
+#uci -q delete network.wan
+#uci -q delete network.wan6
+
+#uci -q commit network
+
+exit 0
+EOF
+
+chmod 755 "$DEFAULT_SETTINGS"
+
 find ./ -name *.orig | xargs rm -f
 find ./ -name *.rej | xargs rm -f
 
