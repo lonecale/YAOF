df7157613b811b00bbdcbe97611c1c0e40fbbac0
lonecale <54177993+lonecale@users.noreply.github.com>
2026-08-16 02:08:07 +0800

    Remove timecontrol repo cloning from get_ready.sh
    Removed cloning of the timecontrol repository from the script.

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -97,8 +97,6 @@ clone_repo $sirpdboy_advancedplus_repo main OpenWrt-Custom/luci-app-advancedplus
 clone_repo $sirpdboy_lucky_repo main OpenWrt-Custom/luci-app-lucky &
 clone_repo $sirpdboy_netspeedtest_repo main OpenWrt-Custom/netspeedtest &
 clone_repo $stevenjoezhang_adguardhome_repo dev OpenWrt-Custom/luci-app-adguardhome &
-clone_repo $kavass168_timecontrol_repo main OpenWrt-Custom/luci-app-timecontrol &
-
 
 (
   clone_repo "$sundaqiang_wolplus_repo" master luci-app-wolplus &&
